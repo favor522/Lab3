3ed5bf587a3ec76f75e09275222c70700f6b7d6a
favor522 <gurin7772@yandex.com>
2024-03-11 00:28:20 +0300

    CPP

---
+++ Lab3_1/Lab3_1/Lab3_1.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>;
+using namespace  std;
+
+int main(){
+	locale::global(locale("Russian_Russia.1251"));
+	srand(time(NULL));
+	char N[5] = { rand() % 10000 + 0 };
+	char *p = N;
+	cout << "Рандомное число: " << p << "\n";
+	return 0;
+}
